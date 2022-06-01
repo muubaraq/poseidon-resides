6dace8be392377071550037f02b2149505babb32
unknown <okechukwuclinton005@gmail.com>
2022-06-01 22:34:09 +0100

    added last slide

--- js/custom.js
+++ js/custom.js
@@ -33,3 +33,20 @@ $(function () {
     }
   });
 });
+
+$(function () {
+  //The color of the selected value changed
+  jQuery("#state-link").on("change", function () {
+    if (jQuery(this).val()) {
+      let colorselect = jQuery(this).find("option:selected").attr("id");
+      jQuery(".bi-circle-fill option").css("color", "#919ca1");
+      jQuery(".bi-circle-fill")
+        .find("option:selected")
+        .css("color", colorselect);
+      jQuery(".bi-circle-fill").css("color", colorselect);
+    } else {
+      jQuery(".bi-circle-fill").css("color", "#919ca1");
+      jQuery(".bi-circle-fill option").css("color", "#919ca1");
+    }
+  });
+});
